59c49113fcbcaaed4f6fc27ae6c6520c3885e662
Jet Rico <jetriconew@gmail.com>
2017-11-19 10:00:19 +0800

    Webpack: babel, css loading, image loading, clean, zip, html

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -1,4 +1,9 @@
-const path = require('path');
+const path = require('path')
+
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const ZipPlugin = require('zip-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   entry: {
@@ -9,5 +14,52 @@ module.exports = {
   output: {
     filename: '[name].js',
     path: path.resolve(__dirname, 'release')
-  }
-};
+  },
+  module: {
+    rules: [
+      { // Babel JS
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader',
+          options: { presets: ['@babel/preset-env'] }
+        },
+        exclude: /(node_modules|bower_components)/
+      },
+      { // Global Styles
+        test: /\.(css|scss)$/,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader' },
+          { loader: 'sass-loader' }
+        ],
+        include: [ path.resolve(__dirname, "../src/content/assets/scss") ]
+      },
+      { // Theme Styles
+        test: /\.(css|scss)$/,
+        use: [
+          { loader: 'style-loader/useable' },
+          { loader: 'css-loader' },
+          { loader: 'sass-loader' }
+        ],
+        include: [ path.resolve(__dirname, "../src/content/themes") ]
+      },
+      { // Images
+        test: /\.(png|jpg)$/,
+        use: [
+          { loader: 'file-loader', options: { outputPath: 'assets/' } }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(['./release','./release.zip']),
+    new CopyWebpackPlugin([
+      { from: './src/manifest.json', to: 'manifest.json' },
+    ]),
+    new ZipPlugin({
+      path: '../',
+      filename: 'release.zip',
+    }),
+    new HtmlWebpackPlugin()
+  ]
+}
