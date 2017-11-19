05ae723c07778f10542e14eb9aaafb1ab9d7de2e
Jet Rico <jetriconew@gmail.com>
2017-11-19 11:04:22 +0800

    manifest.json, working injections, webpack-watch

--- src/content/netgame.js
+++ src/content/netgame.js
@@ -1,17 +1,1 @@
-import ThemeIllustrious from './themes/illustrious.scss'
-import Logo from './assets/logo/128.png';
-
-var headLogo = new Image()
-headLogo.src = Logo
-headLogo.className = 'logo'
-document.querySelector('body').appendChild(headLogo)
-
-let themeApplied = false
-setInterval(() => {
-  if (themeApplied) {
-    ThemeIllustrious.unuse()
-  } else {
-    ThemeIllustrious.use()
-  }
-  themeApplied = !themeApplied
-}, 2000)
+console.log('KC3改 Firefox NETGAME')
