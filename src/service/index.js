7d891cd00bbb581d509e9b30838c276a62e0af9b
Jet Rico <jetriconew@gmail.com>
2018-08-18 18:12:22 +0800

    logging network activity

--- src/service/index.js
+++ src/service/index.js
@@ -7,9 +7,13 @@ RequestListener.listen()
 import NetworkFirefox from './network/firefox.network'
 let netListener = new NetworkFirefox()
 netListener.subscribe('*://*/kcsapi/*', data => {
+  let responseObj = null
   if (data.body.indexOf('svdata=') === 0)
     data.body = data.body.substr(7)
   if (data.body)
-    data.body = JSON.parse(data.body)
-  console.log(data)
+    responseObj = JSON.parse(data.body)
+  if (responseObj)
+    console.log('DATA', data.request.requestBody.formData, responseObj.api_data)
+  else
+    console.log('DATA', data.request.requestBody.formData, data.body)
 })
